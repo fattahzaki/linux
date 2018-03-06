1e2a33bcf62bfab3eb7c5d64ebafa20abf36ef36
Vinicius Costa Gomes <vinicius.gomes@intel.com>
2018-03-06 09:55:28 -0800

    net/sched: Introduce the TBS Qdisc
    TBS (Time Based Scheduler) uses the information added earlier in this
    series (the socket option SO_TXTIME and the new role of
    sk_buff->tstamp) to schedule traffic transmission based on absolute
    time.
    
    For some workloads, just bandwidth enforcement is not enough, and
    precise control of the transmission of packets is necessary.
    
    Example:
    
    $ tc qdisc replace dev enp2s0 parent root handle 100 mqprio num_tc 3 \
               map 2 2 1 0 2 2 2 2 2 2 2 2 2 2 2 2 queues 1@0 1@1 2@2 hw 0
    
    $ tc qdisc add dev enp2s0 parent 100:1 tbs delta 100000 \
               clockid CLOCK_REALTIME sorting
    
    In this example, the Qdisc will provide SW best-effort for the control
    of the transmission time to the network adapter, the time stamp in socket
    are in reference to the clockid CLOCK_REALTIME and packets leave the
    Qdisc "delta" (100000) nanoseconds before its transmission time. It will
    also enable sorting of the buffered packets based on their txtime.
    
    The qdisc will drop packets on enqueue() if their skbuff clockid does not
    match the clock reference of the Qdisc. Moreover, on dequeue(), a packet
    will be dropped if it has expired while being enqueued.
    
    The qdisc also supports the SO_TXTIME deadline mode. For this mode, it
    will dequeue a packet as soon as possible and change the skb timestamp
    to 'now' during tbs_dequeue().
    
    Signed-off-by: Jesus Sanchez-Palencia <jesus.sanchez-palencia@intel.com>
    Signed-off-by: Vinicius Costa Gomes <vinicius.gomes@intel.com>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -792,6 +792,7 @@ enum tc_setup_type {
 	TC_SETUP_QDISC_RED,
 	TC_SETUP_QDISC_PRIO,
 	TC_SETUP_QDISC_MQ,
+	TC_SETUP_QDISC_TBS,
 };
 
 /* These structures hold the attributes of bpf state that are being passed
